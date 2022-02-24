b8d8af737527477d7cb5f3921ad12d6bb3059f29
sehaj-79 <bhasin.seh79@gmail.com>
2022-02-24 15:17:50 +0530

    Notice Adapter Fix Commit

--- app/src/main/java/Adapter/NoticeAdapter.java
+++ app/src/main/java/Adapter/NoticeAdapter.java
@@ -1,5 +1,6 @@
 package Adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,20 +14,28 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.aliferous.bvnfacultyandroid.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
 
 import Model.Notices;
+import Model.User;
 
 public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.MyViewHolder> {
 
     private List<Notices> noticesList;
     private Context activity;
     private FirebaseFirestore firestore;
+    private int MasterAccess;
 
-    public NoticeAdapter(Context mainActivity , List<Notices> noticesList){
+
+    public NoticeAdapter(Context mainActivity , List<Notices> noticesList, int MasterAccess){
         this.noticesList = noticesList;
+        this.MasterAccess = MasterAccess;
         activity = mainActivity;
     }
 
@@ -36,6 +45,7 @@ public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.MyViewHold
         View view = LayoutInflater.from(activity).inflate(R.layout.notice_item , parent , false);
 
         firestore = FirebaseFirestore.getInstance();
+
         return new MyViewHolder(view);
     }
 
@@ -58,7 +68,7 @@ public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.MyViewHold
 
     }
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
 
         Notices notices = noticesList.get(position);
 
@@ -67,6 +77,18 @@ public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.MyViewHold
             holder.notice_title.setText(notices.getTitle());
             holder.notice_desc.setText(notices.getDesc());
             holder.notice_number.setText(""+notices.getCount());
+
+            if (MasterAccess == 0)
+            {
+                holder.notice_delete.setVisibility(View.VISIBLE);
+            }
+
+            holder.notice_delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    deleteTask(position);
+                }
+            });
         }
 
 
@@ -83,7 +105,7 @@ public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.MyViewHold
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
 
-        TextView notice_number,notice_title,notice_desc;
+        TextView notice_number,notice_title,notice_desc,notice_delete;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -91,6 +113,7 @@ public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.MyViewHold
             notice_number = itemView.findViewById(R.id.notice_number);
             notice_title = itemView.findViewById(R.id.notice_title);
             notice_desc = itemView.findViewById(R.id.notice_details);
+            notice_delete = itemView.findViewById(R.id.notice_delete);
 
         }
     }

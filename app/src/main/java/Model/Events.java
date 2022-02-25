4486c28dafd01b562be88bfdc1cfaa3a8d46348e
sehaj-79 <bhasin.seh79@gmail.com>
2022-02-25 23:33:54 +0530

    Events Model Java Commit

---
+++ app/src/main/java/Model/Events.java
@@ -0,0 +1,25 @@
+package Model;
+
+
+public class Events extends NoticeId{
+
+    private String Name;
+
+
+    public Events(String Name){
+        this.Name = this.Name;
+
+    }
+
+    public Events(){
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+}
